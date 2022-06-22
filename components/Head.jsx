dccacf86a3b7223adbe4c75e1fbfb554b3ec6b25
Cassandra <cassandra.tang@hotmail.com>
2022-06-22 13:37:04 +0100

    added meta tags

--- components/Head.jsx
+++ components/Head.jsx
@@ -1,17 +1,29 @@
 import Head from "next/head";
 
 export default function HeadComponent() {
+  const seo = {
+    title: "Cassandra Tang",
+    description:
+      "Product Designer, based in Glasgow. I like crafting intuitive products and meaningful experiences.",
+    image: "/ct-siteimage.png",
+    url: "https://cassandratang.me",
+  };
   return (
     <Head>
-      <title>Cassandra Tang</title>
-      <meta property="og:title" content="Cassandra Tang" key="title" />
-      <meta
-        name="og:description"
-        content="Product Designer, based in Glasgow. I like crafting intuitive products and meaningful experiences."
-      />
-      <meta property="og:image" content="./ct-siteimage.png" />
-      <link rel="icon" href="/favicon.svg" />
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
 
+      <link rel="icon" href="/favicon.svg" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"></meta>
     </Head>
   );
